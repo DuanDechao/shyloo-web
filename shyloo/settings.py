2c2feae0cdf01067910f63cd7838684ab184f44c
段德超 <duandechao@corp.netease.com>
2018-01-10 22:55:36 +0800

    sdfssdfsf

--- shyloo/settings.py
+++ shyloo/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
 	
     'mknp',
 )
@@ -104,6 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- shyloo/urls.py
+++ shyloo/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^mknp/', include('mknp.urls', namespace='mknp', app_name='mknp')),
 	url(r'^media/(?P<path>.*)$',  serve, {"document_root": settings.MEDIA_ROOT}),
+	url(r'^static/(?P<path>.*)$',  serve, {"document_root": settings.STATIC_ROOT}),
 ]
